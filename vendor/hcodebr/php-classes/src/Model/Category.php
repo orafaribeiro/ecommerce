98b589b494f4b0d74e6d9ab9f7769b9867be5102
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-05 21:34:46 -0300

    Paginação

--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -109,6 +109,34 @@ class Category extends Model {
 
 	}
 
+	public function getProductsPage($page = 1, $itemsPerPage = 3)
+	{
+
+		$start = ($page - 1) * $itemsPerPage;
+
+		$sql = new Sql();
+
+		$results = $sql->select("
+			SELECT SQL_CALC_FOUND_ROWS *
+			FROM tb_products a
+			INNER JOIN tb_categoriesproducts b ON a.idproduct = b.idproduct
+			INNER JOIN tb_categories c ON b.idcategory = c.idcategory
+			WHERE c.idcategory = :idcategory
+			LIMIT $start, $itemsPerPage;
+		", array(
+			":idcategory"=>$this->getidcategory()
+		));
+
+		$resultsTotal = $sql->select("SELECT FOUND_ROWS() as nrtotal;");
+
+		return array(
+			"data"=>Product::checkList($results),
+			"total"=>(int)$resultsTotal[0]['nrtotal'],
+			"pages"=>ceil($resultsTotal[0]['nrtotal'] / $itemsPerPage)
+		);
+
+	}
+
 	public function addProduct(Product $product)
 	{
 
