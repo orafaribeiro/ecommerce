5f83c2b5ad5a29e3d36454fb038a850c4b2b2dff
Anthony Ribeiro <anthonyribeiro1910@gmail.com>
2017-09-06 19:06:43 -0300

    Site Cadastro

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -351,6 +351,37 @@ class User extends Model {
 
 	}
 
+	public static function getErrorRegister()
+	{
+
+		$msg = (isset($_SESSION[User::ERROR_REGISTER])) ? $_SESSION[User::ERROR_REGISTER] : '';
+
+		User::clearErrorRegister();
+
+		return $msg;
+
+	}
+
+	public static function clearErrorRegister()
+	{
+
+		$_SESSION[User::ERROR_REGISTER] = NULL;
+
+	}
+
+	public static function checkLoginExist($login)
+	{
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :deslogin;", array(
+			":deslogin"=>$login
+		));
+
+		return (count($results) > 0);
+
+	}
+
 	public static function getPasswordHash($password)
 	{
 
